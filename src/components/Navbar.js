eb5da8ce728ada93c8891032be7812b98f572ae6
Lawlesh Singh <lawleshsingh08@gmail.com>
2022-09-30 02:24:36 +0530

    My first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Navbar=()=> {
+  return (
+    <>
+      <section>
+        <nav className="navbar__nav">
+          <div className="container-fluid">
+            <div className="container">
+              <h1 className="text-success">
+                TV Maze
+              </h1>
+            </div>
+          </div>
+        </nav>
+      </section>
+    </>
+  );
+}
+
+export default Navbar;
